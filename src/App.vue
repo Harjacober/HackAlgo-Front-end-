867ba63fc6b87283361b53e0ba5dd53e66e5f827
Osivwi Okiti <osivwiokiti@gmail.com>
2019-11-27 12:51:56 +0100

    ADD header and home component UI designs

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div id="app">
+    <uiHeader></uiHeader>
     <router-view/>
   </div>
 </template>
+<script>
+import uiHeader from '@/components/header.vue';
 
+export default {
+  components: {
+    uiHeader,
+  },
+};
+</script>
 <style lang="scss">
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
