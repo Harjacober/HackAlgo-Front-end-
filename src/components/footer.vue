45c59bb2c4bb5b4f589a071b14c90110d2e1cf86
Osivwi Okiti <osivwiokiti@gmail.com>
2020-01-21 08:52:20 +0100

    fixed responsiveness on homepage

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,34 +1,22 @@
 <template>
-  <footer class="flex column align-items-center nowrap gen-pad col-lg-6">
-      <div class="flex">
-        <p class="closing-quote">
+  <footer class="w-full
+    px-5 lg:px-20 py-3">
+      <div class="flex lg:w-5/12">
+        <p class="flex text-center lg:w-12/12 text-gray-500">
             <!-- eslint-disable-next-line -->
              At StudentHack, our mission is to help you improve yourself and land your dream job. We have a sizable repository of interview resources for many companies. In the past few years, our users have landed jobs at top companies around the world.
          </p>
       </div>
-      <div>
-          <p>Copyright &copy; 2020 {{name}}. | Priacy Policy</p>
+      <div class="flex justify-center w-auto lg:w-5/12 pt-8">
+          <p class="text-gray-500">Copyright &copy; 2020 {{name}}. | Priacy Policy</p>
       </div>
   </footer>
 </template>
 <style lang="scss" scoped>
     footer {
-        font-size: 14px;
-        a {
-            padding: 5px 0;
-        }
-        .closing-quote {
-            width: 500px;
-            text-align: center;
-            font-size: 16px;
-            color: #969696;
-            padding: 20px;
-        }
-        @media only screen and (max-width: 720px) {
-            .closing-quote {
-                width: 100%;
-            }
-        }
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
     }
 </style>
 <script>
